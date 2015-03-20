52e2be83f89ae790c9bbb06c4c56e431b984009a
Nicholas Rutherford <zgithub@isomorphism.ca>
2015-03-20 16:24:22 -0600

    Cleaned up sentenceSelection

--- hnSummarized/sentenceSelection.py
+++ hnSummarized/sentenceSelection.py
@@ -6,101 +6,129 @@ License: MIT
 import nltk
 import networkx as nx
 from nltk.corpus import stopwords
+
 import re
 
-def word_token(s, STOP_WORDS):
-    """Convert a sentence into a list of words, excluding stop words"""
-    good = []
+def word_tokenize(s, stop_words):
+    """Convert a sentence into a list of words, excluding stop words
+
+    Args:
+        s (str) - A sentence to split into words
+        stop_words ([str]) - A list of words to ignore
+
+    Returns:
+        [str] - The words of the sentence, not including stop_words
+    """
+    quality_words = []
     base = nltk.word_tokenize(s)
     for word in base:
-        if word not in STOP_WORDS:
-            good.append(word)
-    return good
-
-def goodSentences(rawList):
-    sentList = []
-    for sentence in rawList:
-        if sentence.count("]") > 0:
-            continue
-        if sentence.count("[") > 0:
-            continue
-        if sentence.count("{") > 0 :
-            continue
-        if sentence.count("}") > 0 :
-            continue
-        if sentence.count("(") > 0 :
-            continue
-        if sentence.count(")") > 0:
-            continue
-        if sentence.count("|") > 0:
-            continue
-        if sentence.count("=") > 0:
-            continue
-        if sentence.count("%") > 1:
-            continue
-        if sentence.count(">") > 1:
-            continue
-        if sentence.count("<") > 1:
-            continue
-        sentList.append(sentence)
-    return sentList
+        if word not in stop_words:
+            quality_words.append(word)
+    return quality_words
 
 def tokeniseSentences(rawText):
+    """Convert a block of text into a list of sentences
+
+    Args:
+        rawText (str) - A block of text
+
+    Returns:
+        [str] - List of sentences
+    """
     # Remove newlines, sometimes mess up sentence detector
     rawText = rawText.replace("\n", " ")
 
     # Load pre-learned sentence detector, and split into sentences
     sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-    rawSentList = sent_detector.tokenize(rawText)
+    return sent_detector.tokenize(rawText)
 
-    # Select Good sentences
-    sentList = goodSentences(rawSentList)
+def add_nodes(g, sentList):
+    """Add sentence nodes to the graph
 
-    return sentList
-
-
-
-def selectSentences(rawText, K):
-    # Load stop words
-    STOP_WORDS = stopwords.words('english')
-
-    sentList = tokeniseSentences(rawText)
-
-    # Initialize graph
-    g = nx.Graph()
-
-    # Add each sentence as a node
+    Args:
+        g (nx.graph)- The graph
+        sentList ([str]) - List of sentences
+    """
     for sentence in sentList:
         g.add_node(sentence)
 
-    # Determine edge weights
-    for i, sent1 in enumerate(sentList):
-        for j, sent2 in enumerate(sentList):
+def add_edges(g, sentList,stop_words):
+    """Add weighted edges to the graph
+
+    Args:
+        g (nx.graph) - The graph
+        sentList ([str]) - List of sentences
+        stop_words ([str]) - List of words to ignore
+    """
+    # Word tokenize each sentence
+    token_list = []
+    for sent in sentList:
+        token_list.append((sent, word_tokenize(sent, stop_words)))
+
+    # Compute the edge weight for two sentences
+    for i, pair1 in enumerate(token_list):
+        for j, pair2 in enumerate(token_list):
             if i < j:
-                words1 = word_token(sent1, STOP_WORDS)
-                words2 = word_token(sent2, STOP_WORDS)
+                words1 = pair1[1]
+                words2 = pair2[1]
                 wordCount = 0
                 for word in words1:
                     if word in words2:
                         wordCount += 1
-                w = wordCount / float((len(words1) + len(words2))) #((math.log(len(words1))) + math.log(len(words2)))
-                g.add_edge(sent1, sent2, weight =w)
+                w = wordCount / float((len(words1) + len(words2)))
+                g.add_edge(pair1[0], pair2[0], weight=w)
 
-    # Run pagerank and get scores
-    scores = nx.pagerank(g)
+def construct_graph(g, sentList, stop_words):
+    """Add nodes and edges to the graph according to the textRank algorithm
 
-    # Find cutoff score
-    score_list = scores.items()
-    try:
-        min_score = sorted(score_list, key=lambda x : x[1], reverse=True)[K][1]
-    except IndexError:
-        min_score = 0
+    Args:
+        g (nx.graph) - The graph
+        sentList ([str]) - List of sentences
+        stop_words ([str]) - List of words to ignore
+    """
+    add_nodes(g, sentList)
+    add_edges(g, sentList, stop_words)
+
+def text_rank(sentList, stop_words):
+    """Performs the textRank algorithm to obtain 'importance' scores for
+        each sentence.
+
+    Args:
+        sentList ([str]) - List of sentences
+        stop_words ([str]) - List of words to ignore
+
+    Returns:
+        [("str", Float)] - List of sentence, score pairs sorted descending by
+                                score value
+    """
+    g = nx.Graph()
+
+    construct_graph(g, sentList, stop_words)
+    scores = nx.pagerank(g).items()
+    scores = sorted(scores, key=lambda x : x[1], reverse=True)
+    return scores
+
+def summary_para(scores, sentList, K):
+    """Constructs the summary text selecting the K best sentences and
+            formatting them in cronological order
+
+    Args:
+        scores [("str", Float)] - List of sentence, score pairs sorted
+                                    descending by score value
+        sentList ([str]) - List of sentencses
+        K (int) - The number of sentences that the summary should be
+
+    Returns:
+        str - The K-sentence summary
+    """
+    good_sent = [x[0] for x in scores[:K]]
 
     # Return sentences above cutoff in the order they appeared in the text
     toReturn = ""
-    skip = False
+
+    skip = False # Used to insert '[...]' when sentences are skipped
     for sentence in sentList:
-        if scores[sentence] > min_score:
+        if sentence in good_sent:
             if skip:
                 toReturn += " [...] "
             else:
@@ -109,5 +137,22 @@ def selectSentences(rawText, K):
             skip = False
         else:
             skip = True
-    toReturn = re.sub( r'\s+', ' ', toReturn ).strip()
-    return toReturn
+
+    # Remove all excessive whitespace
+    return re.sub( r'\s+', ' ', toReturn ).strip()
+
+def selectSentences(rawText, K):
+    """Summarise text into K sentences using textRank
+
+    Args:
+        rawText (str) - Block of text to be summarized
+        K (int) - Number of sentences that the summary should be
+
+    Returns:
+        str - The K-sentence summary
+    """
+    stop_words = stopwords.words('english')
+    sentList = tokeniseSentences(rawText)
+    scores = text_rank(sentList, stop_words)
+
+    return  summary_para(scores, sentList, K)
