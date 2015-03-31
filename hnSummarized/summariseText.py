1460497065f41447ade4a135727768e4182997fc
Nicholas Rutherford <zgithub@isomorphism.ca>
2015-03-30 20:51:36 -0600

    Don't summarise files you already have

--- hnSummarized/summariseText.py
+++ hnSummarized/summariseText.py
@@ -1,6 +1,7 @@
 from sentenceSelection import selectSentences
 from keywordExtraction import extractKeywords
 import sumUtil
+import os
 
 
 TEXT_DIR = "./hnSummarized/text/"
@@ -12,20 +13,21 @@ NUM_KEYWORDS = 4
 def summariseAll():
     for folder in sumUtil.listDirectory(TEXT_DIR):
         for downFile in sumUtil.listDirectory(TEXT_DIR + folder):
-            print downFile
-            path = TEXT_DIR + folder + "/" + downFile
+            if not os.path.isfile(SUM_DIR + folder + "/" + downFile):
+                print downFile
+                path = TEXT_DIR + folder + "/" + downFile
 
-            rawText = sumUtil.loadFile(path)
+                rawText = sumUtil.loadFile(path)
 
-            # Summarise
-            summary = selectSentences(rawText, NUM_SENTENCES)
+                # Summarise
+                summary = selectSentences(rawText, NUM_SENTENCES)
 
-            # Key Words
-            keyWordsList = extractKeywords(rawText, NUM_KEYWORDS)
-            keyWords = " | ".join(keyWordsList)
+                # Key Words
+                keyWordsList = extractKeywords(rawText, NUM_KEYWORDS)
+                keyWords = " | ".join(keyWordsList)
 
-            toSave = keyWords + "\n" + summary
-            sumUtil.saveAndMakePath(SUM_DIR + folder + "/", downFile, toSave)
+                toSave = keyWords + "\n" + summary
+                sumUtil.saveAndMakePath(SUM_DIR + folder + "/", downFile, toSave)
 
 if __name__ == "__main__":
     summariseAll()
