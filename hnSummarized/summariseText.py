314330b95785d53a9af97fbbf94cee85a31e7961
Nicholas Rutherford <zgithub@isomorphism.ca>
2015-06-23 23:09:36 -0600

    Robust to segfaults

--- hnSummarized/summariseText.py
+++ hnSummarized/summariseText.py
@@ -27,24 +27,70 @@ NUM_SENTENCES = 10
 NUM_KEYWORDS = 4
 
 
+def hasFailedBefore(downfile):
+    """Checks if a file has failed before
+
+    Args:
+        downfile (str) - The current file to check
+
+    Returns:
+        Bool - Wether the file has failed to summarize previously
+    """
+    failed = sumUtil.loadFailed()
+    return downfile in failed
+
+
+def checkIn(downfile):
+    """Mark a file as failing.
+
+    Args:
+        downfile (str) - The current file to check
+
+    Notes:
+        The file is marked as failed, then it is summarized. If the summary
+        runs fine, then the file is removed from the failing list. If the
+        summary runs into a segfault, then the file will remain marked
+        as failed, and will not be tried again.
+    """
+    failed = sumUtil.loadFailed()
+    failed.append(downfile)
+    sumUtil.saveFailed(failed)
+
+
+def checkOut(downfile):
+    """Mark a file as completed succesfully.
+
+    Args:
+        downfile (str) - The current file to check
+    """
+    failed = sumUtil.loadFailed()
+    failed.remove(downfile)
+    sumUtil.saveFailed(failed)
+
+
 def summariseAll():
     for folder in sumUtil.listDirectory(TEXT_DIR):
         for downFile in sumUtil.listDirectory(TEXT_DIR + folder):
             if not os.path.isfile(SUM_DIR + folder + "/" + downFile):
                 print downFile
-                path = TEXT_DIR + folder + "/" + downFile
+                if (not hasFailedBefore(downFile)):
+                    checkIn(downFile)
+                    path = TEXT_DIR + folder + "/" + downFile
 
-                rawText = sumUtil.loadFile(path)
+                    rawText = sumUtil.loadFile(path)
 
-                # Summarise
-                summary = selectSentences(rawText, NUM_SENTENCES)
+                    # Summarise
+                    summary = selectSentences(rawText, NUM_SENTENCES)
 
-                # Key Words
-                keyWordsList = extractKeywords(rawText, NUM_KEYWORDS)
-                keyWords = " | ".join(keyWordsList)
+                    # Key Words
+                    keyWordsList = extractKeywords(rawText, NUM_KEYWORDS)
+                    keyWords = " | ".join(keyWordsList)
 
-                toSave = keyWords + "\n" + summary
-                sumUtil.saveAndMakePath(SUM_DIR + folder + "/", downFile, toSave)
+                    toSave = keyWords + "\n" + summary
+                    sumUtil.saveAndMakePath(SUM_DIR + folder + "/",
+                                            downFile, toSave)
+                    checkOut(downFile)
+
 
 if __name__ == "__main__":
     summariseAll()
